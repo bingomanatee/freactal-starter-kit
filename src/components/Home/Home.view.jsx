4771ff0a696702870475ca01afa59d5cecc2b365
davidedelhart <dave@wonderlandlabs.com>
2018-09-17 19:28:56 -0700

    fixed home page

--- src/components/Home/Home.view.jsx
+++ src/components/Home/Home.view.jsx
@@ -1,5 +1,5 @@
 import { injectState } from 'freactal';
-import { Button } from 'react-md';
+import { List, ListItem } from 'react-md';
 import { withRouter } from 'react-router-dom';
 
 import styles from './Home.module.css';
@@ -8,12 +8,12 @@ import lib from '../../lib';
 // eslint-disable-next-line no-unused-vars
 export default withRouter(injectState(({ state, effects, history }) => (
   <div className={styles.Home}>
-    <h1 className={styles['Home-head']}>Welcome to {lib.SITE_NAME})</h1>
-    <ul>
-      <li><Button flat style={({ color: '#999' })}>Home Page (You are here)</Button></li>
-      <li><Button flat primary onClick={() => history.push('page-one')}>Page One</Button></li>
-      <li><Button flat primary onClick={() => history.push('page-two')}>Page Two</Button></li>
-      <li><Button flat primary onClick={() => history.push('page-three')}>Page Three</Button></li>
-    </ul>
+    <h1 className={styles['Home-head']}>Welcome to {lib.SITE_NAME}</h1>
+    <List className={styles['Home-PageList']}>
+      <ListItem className={styles['Home-PageList__item']} style={({ color: '#CCC' })} primaryText="Home Page (You are here)" />
+      <ListItem className={styles['Home-PageList__item']} onClick={() => history.push('page-one')} primaryText="Page One" />
+      <ListItem className={styles['Home-PageList__item']} onClick={() => history.push('page-two')} primaryText="Page Two" />
+      <ListItem className={styles['Home-PageList__item']} onClick={() => history.push('page-three')} primaryText="Page Three" />
+    </List>
   </div>
 )));
